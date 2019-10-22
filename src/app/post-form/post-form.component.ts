6518c78aa981bbc653018336654e77a99012e70b
Alexander Sobolev <sobolev.ax@gmail.com>
2019-10-23 00:10:39 +0300

    initial

---
+++ src/app/post-form/post-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-form',
+  templateUrl: './post-form.component.html',
+  styleUrls: ['./post-form.component.sass']
+})
+export class PostFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
